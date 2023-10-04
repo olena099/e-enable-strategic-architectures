a8811080871b4d3f71ab01c0cd7d07e7ad08be9f
olena099 <142909583+olena099@users.noreply.github.com>
2023-10-04 05:03:24 -0700

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,66 @@
+
+        print(f"Random Number: {item}")
+    return data
+    main()
+import random
+
+def generate_random_data():
+if __name__ == "__main__":
+def main():
+
+    for item in data:
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()if __name__ == "__main__":
+def main():
+
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+    return data
+    for item in data:
+
+import random
+
+        print(f"Random Number: {item}")
+def generate_random_data():
+
+import random
+
+    data = generate_random_data()
+def generate_random_data():
+def main():
+    main()
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+    for item in data:
+
+
+        print(f"Random Number: {item}")def main():
+    return data
+    data = generate_random_data()
+    main()
+
+
+
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+if __name__ == "__main__":
+import random
+def generate_random_data():
+
+def generate_random_data():
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+
+import random
+        print(f"Random Number: {item}")
+    for item in data:
+    return data
+
+    main()
+
+if __name__ == "__main__":
