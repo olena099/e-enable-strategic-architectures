98b84dab5694ce871e89f29645a870c3c7c03ae5
olena099 <142909583+olena099@users.noreply.github.com>
2023-10-15 13:18:57 -0700

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,13 +1,43 @@
+
+    data.forEach(item => {
+
+
+    for (let i = 0; i < 10; i++) {
 }
-        for (int item : data) {
-        }
-        for (int i = 0; i < 10; i++) {
-
-        }
-            data[i] = (int) (Math.random() * 100) + 1;
+function generateRandomData() {
+    const data = generateRandomData();
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
+        console.log("Random Number: " + item);
     }
-            System.out.println("Random Number: " + item);
-        int[] data = new int[10];
-    public static void main(String[] args) {
+    return data;
+function main() {
+}
+    const data = [];
+    });def generate_random_data():
 
-public class RandomDataGenerator {
+        print(f"Random Number: {item}")
+
+    data = generate_random_data()
+    for item in data:
+    return data
+
+import random
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+
+if __name__ == "__main__":
+def main():
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+
+    return data
+
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+
+def generate_random_data():
+    data = generate_random_data()
+import random
+    main()
