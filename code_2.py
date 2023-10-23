b07f4ac4cbf3531e5174ff8ca8d8607fafa2c46a
olena099 <142909583+olena099@users.noreply.github.com>
2023-10-23 03:04:09 -0700

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,17 +1,49 @@
-
-function main() {
-}
-
-        data.push(Math.floor(Math.random() * 100) + 1);
-}
-function generateRandomData() {
-main();
-    data.forEach(item => {
-    const data = generateRandomData();
-    for (let i = 0; i < 10; i++) {
-    const data = [];
-    return data;
-    }
         console.log("Random Number: " + item);
+}
+}
+function main() {
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    const data = [];
+    const data = generateRandomData();
+
+    }
+main();
+
+function generateRandomData() {
+    data.forEach(item => {
+    return data;
+    });
+}
+    return data;
+function generateRandomData() {
+    for (let i = 0; i < 10; i++) {
+function main() {
+        console.log("Random Number: " + item);
+    const data = [];
+
+    data.forEach(item => {
+main();
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+    }
+
+    const data = generateRandomData();    for (let i = 0; i < 10; i++) {
 
     });
+    }
+function main() {
+main();
+}
+function generateRandomData() {
+
+}
+    data.forEach(item => {
+    const data = generateRandomData();
+
+    return data;
+    const data = [];
+        console.log("Random Number: " + item);
+        data.push(Math.floor(Math.random() * 100) + 1);
