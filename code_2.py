c6b446e47eb564a92ace6c338e85d5e9dc846331
olena099 <142909583+olena099@users.noreply.github.com>
2023-10-24 09:20:55 -0700

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,49 +1,14 @@
-        console.log("Random Number: " + item);
-}
-}
-function main() {
-    for (let i = 0; i < 10; i++) {
-        data.push(Math.floor(Math.random() * 100) + 1);
 
-    const data = [];
-    const data = generateRandomData();
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+        print(f"Random Number: {item}")
 
-    }
-main();
+def generate_random_data():
+    for item in data:
+if __name__ == "__main__":
 
-function generateRandomData() {
-    data.forEach(item => {
-    return data;
-    });
-}
-    return data;
-function generateRandomData() {
-    for (let i = 0; i < 10; i++) {
-function main() {
-        console.log("Random Number: " + item);
-    const data = [];
+    main()
+    data = generate_random_data()
 
-    data.forEach(item => {
-main();
-}
-        data.push(Math.floor(Math.random() * 100) + 1);
-    });
-    }
-
-    const data = generateRandomData();    for (let i = 0; i < 10; i++) {
-
-    });
-    }
-function main() {
-main();
-}
-function generateRandomData() {
-
-}
-    data.forEach(item => {
-    const data = generateRandomData();
-
-    return data;
-    const data = [];
-        console.log("Random Number: " + item);
-        data.push(Math.floor(Math.random() * 100) + 1);
+    return data
+import random
