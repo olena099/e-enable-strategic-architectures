c9c486d435ab30450c3d652925e8acd504593f70
olena099 <142909583+olena099@users.noreply.github.com>
2023-10-25 15:38:42 -0700

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,14 +1,29 @@
-
-    data = [random.randint(1, 100) for _ in range(10)]
-def main():
+    data = generate_random_data()
         print(f"Random Number: {item}")
 
-def generate_random_data():
-    for item in data:
-if __name__ == "__main__":
-
-    main()
-    data = generate_random_data()
-
+    data = [random.randint(1, 100) for _ in range(10)]
     return data
-import random
+    main()
+
+
+
+if __name__ == "__main__":
+def generate_random_data():
+def main():
+    for item in data:
+import random}
+        console.log("Random Number: " + item);
+    return data;
+main();
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+    const data = generateRandomData();
+
+    }
+function generateRandomData() {
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
+function main() {
+}
+    data.forEach(item => {
